e11b6ac0d7431eb80b10cbfee60ecb9064cc3e4a
omair2200 <153764448+omair2200@users.noreply.github.com>
2024-04-04 17:15:33 +0300

    Added async frameworok

--- cat_fact_project/cat_facts/views.py
+++ cat_fact_project/cat_facts/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
-import requests
+import aiohttp
+import asyncio
 from django.views.decorators.csrf import csrf_exempt
 
 last_fact = None
@@ -8,20 +9,26 @@ last_fact = None
 def health_check(request):
     return JsonResponse({'status': 'ok'}, status=200)
 
-
-@csrf_exempt
-def fetch_fact(request):
-    global last_fact
+async def fetch_data():
     try:
         url = "https://cat-fact.herokuapp.com/facts"
-        response = requests.get(url)
-        data = response.json()
-        # If data is received, update the last fetched fact and return success response
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+                return data
+    except Exception as e:
+        raise e
+
+@csrf_exempt
+async def fetch_fact(request):
+    global last_fact
+    try:
+        # Fetch data asynchronously
+        data = await fetch_data()
         if data:
             last_fact = data[0]['text']
             return JsonResponse({'success': True})
         else:
-            # If no data is received, return a 404 error with error message
             return JsonResponse({'success': False, 'error': 'No facts found'}, status=404)
     except Exception as e:
         return JsonResponse({'success': False, 'error': str(e)}, status=500)
@@ -30,7 +37,6 @@ def fetch_fact(request):
 def get_fact(request):
     global last_fact
     try:
-        # If last_fact is available, return it
         if last_fact:
             return JsonResponse({'fact': last_fact})
         else:
