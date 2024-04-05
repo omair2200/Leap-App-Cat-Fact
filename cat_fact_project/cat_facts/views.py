f0b136ab70fc93c667d317e95f9175341c1c3490
omair2200 <153764448+omair2200@users.noreply.github.com>
2024-04-05 21:14:24 +0300

    Update views.py

--- cat_fact_project/cat_facts/views.py
+++ cat_fact_project/cat_facts/views.py
@@ -16,7 +16,7 @@ def fetch_fact(request):
 def get_fact(request):
     try:
         last_fact = CatFact.objects.latest('fetched_at')
-        CatFact.objects.all().delete()
+        CatFact.objects.all().delete() #Added this for multiple trial and error testing
         return JsonResponse({'fact': last_fact.fact})
     except CatFact.DoesNotExist:
         return JsonResponse({'error': 'no_task_has_been_queued_yet'})
